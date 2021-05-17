dadf4d3f9d8f832a88e98a10978cf32ff7c292d2
Thiri Hsu Myat  Aung <thirihsumyataung@thiris-imac.myfiosgateway.com>
2021-05-17 03:19:35 -0400

    May 17 commit 1

---
+++ 18-cocktails/README.md
@@ -0,0 +1,6 @@
+# Cocktails 
+
+- Check the api endpoint -> we used two api endpoints in this project: searched by id and searched by name 
+API : https://www.thecocktaildb.com/api.php
+
+- project 18 : Cocktails  [![Netlify Status](https://api.netlify.com/api/v1/badges/2fa25b36-b8e4-499e-a655-9e5f24d9dcb7/deploy-status)](https://app.netlify.com/sites/cocktail-project-practice-by-thirihsumyataung/deploys)    https://cocktail-project-practice-by-thirihsumyataung.netlify.app/
---
+++ 18-cocktails/src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title"> About Cocktails </h1>
+      <p>
+        A cocktail is a term relating to any type of alcoholic mixed drink. Most commonly, cocktails are either a combination of spirits, or one or more spirits mixed with other ingredients such as fruit juice, flavored syrup, or cream. Cocktails vary widely across regions of the world, and many websites publish both original recipes and their own interpretations of older and more famous cocktails.
+ </p><p>
+The origins of the word cocktail have been debated. The first written mention of cocktail as a beverage appeared in The Farmers Cabinet, 1803 in the United States. The first definition of a cocktail as an alcoholic beverage appeared three years later in The Balance and Columbian Repository (Hudson, New York) May 13, 1806.[4] Traditionally, cocktail ingredients included spirits, sugar, water and bitters,[5] however, this definition evolved throughout the 1800s, to include the addition of a liqueur . 
+      </p>
+  </section>
+  )
+}
+
+export default About
